41982889eafc26b18f58a1f20ea77d1f283e86e2
pityugameplay <pityugameplay@gmail.com>
2024-01-26 22:42:52 +0100

    make lora downloads failsafe

--- launch.py
+++ launch.py
@@ -76,7 +76,10 @@ def download_models():
     for file_name, url in embeddings_downloads.items():
         load_file_from_url(url=url, model_dir=path_embeddings, file_name=file_name)
     for file_name, url in lora_downloads.items():
-        load_file_from_url(url=url, model_dir=path_loras, file_name=file_name)
+        try:
+            load_file_from_url(url=url, model_dir=path_loras, file_name=file_name)
+        except:
+            continue
     for file_name, url in vae_approx_filenames:
         load_file_from_url(url=url, model_dir=path_vae_approx, file_name=file_name)
 
