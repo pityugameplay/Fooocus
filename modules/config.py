e7922921b81e2c4ab6bb6461bdd56a4f253ab394
pityugameplay@gmail.com <pityugameplay@gmail.com>
2024-02-26 20:48:44 +0100

    add checkpoint

--- modules/config.py
+++ modules/config.py
@@ -253,7 +253,7 @@ checkpoint_downloads = get_config_item_or_set_default(
     key='checkpoint_downloads',
     default_value={
         "juggernautXL_version6Rundiffusion.safetensors": "https://huggingface.co/lllyasviel/fav_models/resolve/main/fav/juggernautXL_version6Rundiffusion.safetensors",
-        # "sdxxxl_v30.safetensors": "https://drive.usercontent.google.com/download?id=1zbelIborfWCv4g3tF1cOJ4wlUCFJi3As&export=download&authuser=1&confirm=t&uuid=470624a6-0546-42af-bdfb-db267f3a9a87&at=APZUnTVumcx6V7owoYteSDdgJ4lP%3A1707583870938"
+        "sdxxxl_v30.safetensors": "https://drive.usercontent.google.com/download?id=1zbelIborfWCv4g3tF1cOJ4wlUCFJi3As&export=download&authuser=1&confirm=t&uuid=470624a6-0546-42af-bdfb-db267f3a9a87&at=APZUnTVumcx6V7owoYteSDdgJ4lP%3A1707583870938"
         # "realisticVisionV60B1_v51VAE.safetensors": "https://civitai.com/api/download/models/130072?type=Model&format=SafeTensor&size=full&fp=fp16",
     },
     validator=lambda x: isinstance(x, dict) and all(isinstance(k, str) and isinstance(v, str) for k, v in x.items())
